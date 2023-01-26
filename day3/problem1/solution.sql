3f328bf4cf26d08ebd3b85cf2e20966223931037
billcates <m.billcates@gmail.com>
2023-01-26 13:28:47 +0530

    updated solution

--- day3/problem1/solution.sql
+++ day3/problem1/solution.sql
@@ -1,11 +1,11 @@
-with final as(
+with temp as(
     select a.*,b.name as name 
     from orders a 
     join products b on a.product_id=b.id
 ),
-out as(
+final as(
     select a.order_id,a.customer_id,concat(a.name,' ',b.name) as combination
-    from final a join final b on a.order_id=b.order_id and a.product_id<b.product_id
+    from temp a join temp b on a.order_id=b.order_id and a.product_id<b.product_id
 )
-select combination,count(1) from out
+select combination,count(1) from final
 group by combination
